92c581c9590780f162db3ac6fb94c2c933194f03
@brodycj - C. Jonathan Brody <cj@brodycj.com>
2024-09-13 16:40:26 -0400

    export-create-cell-fns

--- src/lib.rs
+++ src/lib.rs
@@ -7,4 +7,6 @@ pub mod load_test;
 pub mod scl {
     // ---
     pub use crate::sclmanager::SCLRef;
+    pub use crate::sclmanager::create_cell_with_links;
+    pub use crate::sclmanager::create_cell_with_text_only;
 }
