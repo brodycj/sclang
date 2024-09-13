0094f856a961f9f003ecc9dd1f78e89738bf93b4
@brodycj - C. Jonathan Brody <cj@brodycj.com>
2024-09-13 16:42:57 -0400

    fixup-fmt

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ pub mod load_test;
 
 pub mod scl {
     // ---
-    pub use crate::sclmanager::SCLRef;
     pub use crate::sclmanager::create_cell_with_links;
     pub use crate::sclmanager::create_cell_with_text_only;
+    pub use crate::sclmanager::SCLRef;
 }
