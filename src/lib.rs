8e55ac5cfe3a8f44fdedb31b569c7dc68ad9f55e
@brodycj - C. Jonathan Brody <cj@brodycj.com>
2024-09-15 12:14:30 -0400

    ..

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,10 @@ mod sc_record_manager;
 
 pub mod sclang;
 
-pub use crate::sc_record_manager::enable_feature;
-pub use crate::sc_record_manager::is_debug_enabled;
 pub use crate::sc_record_manager::SCRecordRef;
+
+pub use crate::sc_record_manager::enable_feature;
+
+pub use crate::sc_record_manager::is_debug_enabled;
 
 pub mod load_test;
