ea07b17820216f0b61c4beba647572db06ead295
@brodycj - C. Jonathan Brody <cj@brodycj.com>
2024-09-14 23:44:16 -0400

    remove-xxx

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,6 @@ pub mod sclang;
 pub mod load_test;
 
 pub mod scl {
-    // ---
     pub use crate::sclmanager::create_cell_with_links;
     pub use crate::sclmanager::create_cell_with_text_only;
     pub use crate::sclmanager::enable_feature;
